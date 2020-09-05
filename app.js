5c0bd85c393d3f96f9ab1dd0ab16035fb4ac0866
Igor Yagushevskij <yagushevskij@gmail.com>
2020-09-05 12:50:32 +0300

    added crush test

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
